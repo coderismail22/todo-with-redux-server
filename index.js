55caf7fa9798dcfbae4db2ce8f932ef83772d97c
ismail <coderismail22@gmail.com>
2024-08-15 16:50:34 +0600

    test

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb://localhost:27017`;
+const uri =
+  "mongodb+srv://todo-with-redux:HIXLGCNgXL5Qu2KE@cluster0.4in3v8j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -20,16 +21,18 @@ const client = new MongoClient(uri, {
 const run = async () => {
   // await client.connect();
   try {
-    const db = client.db('todo');
-    const taskCollection = db.collection('tasks');
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-    // app.get('/tasks', async (req, res) => {
-    //   const cursor = taskCollection.find({});
-    //   const tasks = await cursor.toArray();
-    //   res.send({ status: true, data: tasks });
-    // });
+    const db = client.db("todo-with-redux");
+    const taskCollection = db.collection("tasktest");
 
-    app.get('/tasks', async (req, res) => {
+    app.get("/tasks", async (req, res) => {
       let query = {};
       if (req.query.priority) {
         query.priority = req.query.priority;
@@ -39,28 +42,28 @@ const run = async () => {
       res.send({ status: true, data: tasks });
     });
 
-    app.post('/task', async (req, res) => {
+    app.post("/task", async (req, res) => {
       const task = req.body;
       const result = await taskCollection.insertOne(task);
+      console.log(result);
       res.send(result);
     });
 
-    app.get('/task/:id', async (req, res) => {
+    app.get("/task/:id", async (req, res) => {
       const id = req.params.id;
       const result = await taskCollection.findOne({ _id: ObjectId(id) });
       // console.log(result);
       res.send(result);
     });
 
-    app.delete('/task/:id', async (req, res) => {
+    app.delete("/task/:id", async (req, res) => {
       const id = req.params.id;
       const result = await taskCollection.deleteOne({ _id: ObjectId(id) });
-      // console.log(result);
       res.send(result);
     });
 
     // status update
-    app.put('/task/:id', async (req, res) => {
+    app.put("/task/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const task = req.body;
@@ -83,8 +86,8 @@ const run = async () => {
 
 run().catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Server is running!");
 });
 
 app.listen(port, () => {
